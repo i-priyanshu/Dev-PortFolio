891b7fe59b72a52139b207f0a45a2b0687fc92b3
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-04-07 00:40:19 +0530

    :art: Updated Style

---
+++ portfolio/src/components/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "../styles/home.scss";
+import { NameSvg } from "./svgs";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="center">
+        <NameSvg />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
